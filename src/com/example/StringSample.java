bfc7ce4774a3716560ea2a3a798381665e603065
skarthivelan <skarthivelan@gmail.com>
2023-01-15 20:22:54 +0000

    string samples
    string samples

--- src/com/example/StringSample.java
+++ src/com/example/StringSample.java
@@ -5,8 +5,13 @@ public class StringSample {
 	
 	public static void main(String[] args) {
 		String random = "Hello World";
-		
 		System.out.println(random);
+		
+		String randomNew = new String("Java String");
+		System.out.println(randomNew);
+		
+		String randomNew1 = randomNew.intern();
+		System.out.println(randomNew1);
 	}
 
 }
