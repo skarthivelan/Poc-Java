6d065712a70924ec9b5e19a9dcdc0ca53978ac12
skarthivelan <skarthivelan@gmail.com>
2023-01-16 19:15:45 +0000

    Java String samples
    Java String samples

--- src/com/example/StringSample.java
+++ src/com/example/StringSample.java
@@ -12,6 +12,12 @@ public class StringSample {
 		
 		String randomNew1 = randomNew.intern();
 		System.out.println(randomNew1);
+		
+		String splitStr = randomNew.substring(5, randomNew.length());
+		System.out.println(splitStr);
+		
+		char[] strArray = randomNew.toCharArray();
+		System.out.println(strArray[0]);
 	}
 
 }
